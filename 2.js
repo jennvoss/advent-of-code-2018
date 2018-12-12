101d451710af01b7b10e16ba6634e7ec67c252d0
Voss, Jennifer (ELS-PHI) <j.voss@elsevier.com>
2018-12-11 22:23:11 -0500

    day 2

---
+++ 2.js
@@ -0,0 +1,52 @@
+const input = ['ivyhczwokexltwhsfamqprbnuy','ivjhcjdokexltwwsfamqpabnuy','ivjhczdokebltwgsfydqprbnuy','ivjhcadokexftogsfamqprbnuy','idjhczdokexltwgsfacqprbnuh','ivjhgzookexltwgsfamqjrbnuy','uvjhctdokexltwosfamqprbnuy','ivrhczdokexltwhzfamqprbnuy','ivjhczuxkexltwgsfamqprbney','ivjhczdokemltwgsfadnprbnuy','ifjhczdokexltwgsfamqprbkuf','ivjhkzdokltltwgsfamqprbnuy','ivjuczdhkexltwgsfamqprtnuy','ivjhjzdxkexltwgsfapqprbnuy','ivlhczdoxexltwgsfamqprgnuy','ivjhczdoknxltwgssamqsrbnuy','ivjhczdokexltwgswadqprbruy','ivjhczdokexthwgsfampprbnuy','uvjhczrozexltwgsfamqprbnuy','ivolczdokexltwgsffmqprbnuy','ivjhczibkexltwgsfamoprbnuy','ivjhczdokefltmgsfamqprbouy','ivjhczdobexltngsfamsprbnuy','ivjhczdokexltwvsfxqqprbnuy','dvjhczdokexucwgsfamqprbnuy','kvjhszkokexltwgsfamqprbnuy','ivjhczdokexrtegsfamqprbnus','ivjhctdokexltwglgamqprbnuy','ivjhczdozexutwgsfamqdrbnuy','ivjhczqokemltwgsfakqprbnuy','uvjhczdokexlqwgsfadqprbnuy','ivjhczdohexltwglffmqprbnuy','izjhczdokexltwgsfamqprbsqy','iajhczdokwxltwgjfamqprbnuy','ivjfczdokexllwgslamqprbnuy','ivjhczdoyexltwgsfamqdrbnxy','ivjhczdokekwtwssfamqprbnuy','ivjhcodokexltwgsfamqirxnuy','ihjhhzdokexltwgsfamqlrbnuy','ivjdpzdokexltwfsfamqprbnuy','ivjhcpdokexltwgsfamqqrbruy','qvjhcziokexltwgsfamqprbnny','ivohczdomexltwgsfkmqprbnuy','ivjhczhokhxlywgsfamqprbnuy','ivjhczdokexltwgmffmqprbruy','ivjhczdokqxltwgcfamqprbnyy','ivjhczdokepltwgsfamcprbnay','ivjhczdokexltwgseamqpmbnua','ivjzczdokexltwgszamqplbnuy','ivjhczpokexltwgvfgmqprbnuy','idjhczdokexltwgsxamqprbndy','ivjhczdxkexltwgcgamqprbnuy','ivjhczdckexatpgsfamqprbnuy','ivjrczdorexltwgsfamqprbnvy','ivjoczdokexltwgswamqprbtuy','iylhczdokexltwgsfamqpxbnuy','imxhczdokkxltwgsfamqprbnuy','ivvhczdoktxltwgsfamaprbnuy','ivyhczdokexltwhsfayqprbnuy','ivjhcrdokexltegsfamqprbnum','rvjhezdokexltwgsoamqprbnuy','ivjzczdokexlbwgsfkmqprbnuy','ivjhczdokelltwgsyamqprbnoy','ixjhczdorexltwgsfamqprbuuy','ivjhczpokexdtwglfamqprbnuy','ivjhczdokexltwgfgamcprbnuy','ikjhczdokexltwgsfamqirbnux','ivjhczdopjxltwgsfamqprbnny','ivchczdokexltwgniamqprbnuy','ivjhczdooeqltwgsfamqprbniy','ivjhcldonexltwgbfamqprbnuy','ixjhczdokehltwgsfamqprbnuf','ivjhczdckefltwgsfamqppbnuy','ivjhczdoqrxltwgsfamqprbnun','ivjhczdokcxltwgmfarqprbnuy','ivjhcziorexltqgsfamqprbnuy','ivjhwzdokexltwgnfamqprbcuy','ivjhczdoqexltwgsfazqprunuy','iijhczdokexltwgsyamqprbnug','ivjhczdokexltwgxfamhprbnry','ivjhczdakexltwgsfaeqlrbnuy','ivjhqzdokehltwgsfampprbnuy','ivjhczdokexltwlsfpmyprbnuy','ivjhfzdoktxltwgsfamzprbnuy','ivlhvzdokexltwgsvamqprbnuy','ivjhczdbkexltwgsaamqprfnuy','ivahcedokexltigsfamqprbnuy','cvjhczdokexltwgsfamapibnuy','ivjhczkokbxltwgsfbmqprbnuy','pvjuczdnkexltwgsfamqprbnuy','iyjhczdckexotwgsfamqprbnuy','ivjhzzdokvxltwgsfamqprbnuo','ivjhczdobexltwgsxamqprbnry','ivjhczdokexltwgsfaprprbnub','ivjhczdokexltwgofarqprbkuy','ivjhczdokexltwgbfymqprbnhy','ibjhczdokexltwgsfkmqpvbnuy','ivjhczdzkexlywgsfacqprbnuy','hvdhczdokexltwglfamqprbnuy','ivjhczdokexrtwgsfamqprbsuh','ivjhczhokexltngsfamqpjbnuy','ivjhcsjokexltwgsfaeqprbnuy','ivjmczdokexltmgsfamqpbbnuy','wvjhczdokexltwgsfamkpkbnuy','icjhpzdoaexltwgsfamqprbnuy','ivjmczdhkexltwgsfzmqprbnuy','ivjhczdokexytwgsfamqprbwug','ikjhczdjkexljwgsfamqprbnuy','ivjvcdmokexltwgsfamqprbnuy','ivjhazdorixltwgsfamqprbnuy','ivchczdokexltwgsfamzprenuy','ivjcczdokexlttgsfamqpmbnuy','ibchgzdokexltwgsfamqprbnuy','ivjhczdokepltwgsfamqpeenuy','ivjnwzdokexlrwgsfamqprbnuy','ivjhczdokexitwgsfadqlrbnuy','icjhcrdokexltwgsfamqkrbnuy','ivngczdokexltwgsfamqprbyuy','ivjhuudokexlvwgsfamqprbnuy','ivjhczdnkexltwgsfhmqpxbnuy','itjhczdokexltwvsfamgprbnuy','ivjhcddokexltwgsfakqprbnny','ivjhuzdojexltwfsfamqprbnuy','idjhczdokexltwgsfamqukbnuy','ivjhczdokexlzigsfamqprbngy','ivjwczdokexltwgufamqprbnuo','iijhczdokexltwfsfadqprbnuy','ivjhczdukexdtwgsfamqpsbnuy','idjhczdokexllwgssamqprbnuy','zvjhczdokexrtwgsfamqplbnuy','ivphczdofexltwgefamqprbnuy','ivhhczdokexlpwgsjamqprbnuy','ivjhczdovexltwgsfamqprhnuj','ivjhczdoklxltwgseamqprlnuy','ivjhcqdokexltngsfamqprdnuy','ivjhczdoifxltagsfamqprbnuy','izjhczdokexltwjsramqprbnuy','psjhczdokexlgwgsfamqprbnuy','ivjhcadokexltwgsfsmqwrbnuy','ivjhczdokexltwgsfawqiibnuy','ivjhczkokexhtwgsfamqprbnuk','ivjhcmdukexltwgsfamvprbnuy','ivjlczdokexltwgsfamquibnuy','ivjhczdokexntwgyfamqprbniy','ivjhczdokexltwlsfafqprbnuc','ivjhczdosexltrtsfamqprbnuy','ivjhcznokexbtwgsfafqprbnuy','ivwtczdotexltwgsfamqprbnuy','ivjhvzdokexltigsoamqprbnuy','ivjhcmdokexltwasfamqirbnuy','ivthczdokexltwgsfaydprbnuy','ivjhwzdskexltwgsfamqprbnus','icjhczdosuxltwgsfamqprbnuy','ivjhczdokexltwgstamqbrmnuy','iejhczuoktxltwgsfamqprbnuy','ivjhczdokeqltwgskamqprbniy','ivjhlzdokexltugsfamqprbpuy','iwjqczdckexltwgsfamqprbnuy','ivjhwzdokexluwgsfxmqprbnuy','ivjhczdokexltwgwfwmqprbguy','gvjhczkokexltwgsfgmqprbnuy','ivjhczdoyexlhwgsfamqprbnoy','cvjhczdokexltwgsfomqprinuy','vvmhczdokexltwgsfamqprbnun','vvjhczdokexltwgsftmfprbnuy','ivkhckdokhxltwgsfamqprbnuy','iyjhczdkkexltjgsfamqprbnuy','ivlhczdokexltwgsfamqyrbhuy','tvjhmzdokexltwgsfamqorbnuy','ivjhczdokexltwvsfamqprbnxi','ivjhczdowexltwgswamqerbnuy','wvjiczdomexltwgsfamqprbnuy','ivjpizdokexltwgvfamqprbnuy','ivjhuzdokexlzwgspamqprbnuy','ivjhczdokeyltwgkfamqprdnuy','jvjhczdokexlnwgsfamqirbnuy','ivjheidokexltwvsfamqprbnuy','mvjhczdokexltwgsfamqyrsnuy','ivjhazdykexltwgsramqprbnuy','ivjkcodokexltwgsxamqprbnuy','ikjhczdoktxltwgpfamqprbnuy','ivjhyzdfkexmtwgsfamqprbnuy','ivohczdokexltugsfamqprynuy','ivjkczdqkexltwgshamqprbnuy','ivjhczdokexltwgskamqynbnuy','icjhczdokexltwgofamrprbnuy','ivjhlzdokealtwgsfamqsrbnuy','ivehczdybexltwgsfamqprbnuy','ovjhczdokexltwgsfamqirbnuo','ivjoczdokexltwgsfamqurbnty','ivjmczdokexltwgsfrmqprnnuy','ivjhczdowpxltwgbfamqprbnuy','ivjhczdokexltwfsfamqkrgnuy','ivjhwzdokexltwgsfavqprbnuq','jvjhczdokexltwgsiamqprbnny','ivjhlzdouexltwfsfamqprbnuy','ivjhczdokexltwgsfamqbrbnlv','iwjhczdokexltwgsfapqprbnqy','idjhczdokexltwgsaamqrrbnuy','ivjhjzdopepltwgsfamqprbnuy','ivjmczdokejltwgsfamqpbbnuy','ivjhczdokexltwgsuamdprvnuy','injhczdokexltwgefamqurbnuy','iujhczdokexltwgsaamqjrbnuy','ivjhczdokexltwgvfaaqprbnly','ivehczdokexltwgsfamqppbnui','ivxhczdodexltwgsfamqplbnuy','ivjhczfokexltwgsfamqpwbauy','ivjhcztwkexhtwgsfamqprbnuy','ivjeczdokexltygsfmmqprbnuy','ivjhchdokexltwgsmameprbnuy','ivkhczdoklxltwggfamqprbnuy','ivjhczdzkexltwhsfamqprjnuy','ivjhcedokeultngsfamqprbnuy','ivjhczdokexvtwgseabqprbnuy','ivjhczdooexltlgsfamqpibnuy','ivjgczvosexltwgsfamqprbnuy','ivlhczwokexltwgsfamqmrbnuy','lvjhczdokexutwgsfamrprbnuy','ivahczdokexpdwgsfamqprbnuy','ivjhcznokexltwhsfamqpnbnuy','ivjhczdpkyxltwgbfamqprbnuy','ivjhnzdokexltwgsftmqprinuy','ivihczdokexltnhsfamqprbnuy','ivjhcbdokevltwgsfamqprbauy','hgjoczdokexltwgsfamqprbnuy','dvjhczdckexltwgsfamqpybnuy','ivjhcadokesltwgsfsmqwrbnuy','ivjhwzdokexlttgsfamqprbney','ivjhcidokexltwgofamqfrbnuy','ivokwzdokexltwgsfamqprbnuy','ivjiczdokexltwgsfaqqarbnuy','ivjhczdokexqtwfsfamgprbnuy','ivjhczdokealtwgsfamqerbnqy','ivjhczdskexltwgsfamqprznuu','ivjhwzdokexltwjsfdmqprbnuy','ivjhczaokexlzwgsfamqprbnus','ivjhczdokexltwosfamqnrbnux','ivjhczdokexlqwgsfamwprcnuy','ivjhczdqkexltwgswamqpcbnuy','ijjhczdokexnttgsfamqprbnuy','ivjhcedckexltwgsfamqprbnpy','ivjhczdokeyltwgsfamqshbnuy','ivjhczdokexltsgsfamqpmznuy','ivjlczdtkeiltwgsfamqprbnuy','ivjhczdokexltwgsfkmtprbnby','ivjhnzdozexltwgsfamqprbnuc','xqjxczdokexltwgsfamqprbnuy','ivjhczdokeyltwgsfamqnrbnuw','ivjwczgokexltwgsfamvprbnuy'];
+
+function part1(arr) {
+  let twice = 0;
+  let thrice = 0;
+
+  arr.forEach(el => {
+    let matches = {};
+    el.split('').sort().reduce((a,b) => {
+      if (a === b) matches[b] = matches[b] ? ++matches[b] : 2;
+      return b;
+    }, 0);
+
+    Object.keys(matches).some(key => {
+      if (matches[key] === 2) {twice++; return true;}
+    });
+    Object.keys(matches).some(key => {
+      if (matches[key] === 3) {thrice++; return true;}
+    });
+
+  });
+  return twice*thrice;
+}
+// console.log(part1(input));
+
+function part2(arr) {
+  let matchingItems;
+  const doLettersMatch = (a,b) => {
+    let matches = 0;
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] === b[i]) matches++;
+    }
+
+    if (matches === a.length-1) {
+      matchingItems = [a,b];
+      return true;
+    }
+  }
+
+  for (let i = arr.length-1; i >=0; i--) {
+    let match = arr.some((el, idx) => {
+      return idx === i ? false : doLettersMatch(el, arr[i]);
+    });
+    if (match) {return matchingItems} else {arr.pop()};
+  }
+}
+// console.log(part2(input));
+
+module.exports = {
+  part1: part1,
+  part2: part2
+};
