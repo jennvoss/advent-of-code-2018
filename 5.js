44fa1cf6fdee66aa2af989e2020bfe94da05fe96
Voss, Jennifer (ELS-PHI) <j.voss@elsevier.com>
2018-12-16 19:41:50 -0500

    day 5 refactor

--- 5.js
+++ 5.js
@@ -4,38 +4,33 @@ const letterMatch = (a,b) => a.toLowerCase() === b.toLowerCase();
 const polarityMatch = (a, b) => a === b;
 const reactUnits = (arr) => {
   return arr.reduce((a, b) => {
-  let lastLetter = a.substr(a.length - 1);
-  if (!lastLetter) return b;
+    let lastLetter = a.substr(a.length - 1);
+    if (!lastLetter) return b;
 
-  if (letterMatch(lastLetter, b) && !polarityMatch(lastLetter, b)) {
-    a = a.substr(0, a.length - 1)
-  } else {
-    a += b;
+    if (letterMatch(lastLetter, b) && !polarityMatch(lastLetter, b)) {
+      a = a.substr(0, a.length - 1)
+    } else {
+      a += b;
+    }
+    return a;
   }
-  return a;
-})};
+)};
 
 function part1(str) {
   return reactUnits(str.split('')).length;
 }
 // console.log(part1(input));
 
-const removeEntries = (str, l) => {
-  return str.split('').filter(x => x.toLowerCase() !== l);
-};
-
 function part2(str) {
   const reacted = reactUnits(str.split(''));
   const uniq = new Set(reacted.split('').map(l => l.toLowerCase()));
-  let shortestPolymer;
+  const removeL = (str, l) => str.split('').filter(x => x.toLowerCase() !== l);
+  const lengthWithoutL = (str, l) => reactUnits(removeL(str, l)).length;
 
-  uniq.forEach(l => {
-    let testStr = removeEntries(reacted, l);
-    let len = reactUnits(testStr).length;
-    if (!shortestPolymer || len < shortestPolymer) {shortestPolymer = len};
+  return Array.from(uniq).reduce((a,b) => {
+    if (typeof a !== 'number') {a = lengthWithoutL(reacted, a)}
+    return Math.min(a, lengthWithoutL(reacted, b));
   });
-
-  return shortestPolymer;
 }
 // console.log(part2(input));
 
