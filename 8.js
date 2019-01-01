0a09b8165ab638098a27d20656992549bbd8d252
Voss, Jennifer (ELS-PHI) <j.voss@elsevier.com>
2019-01-01 16:07:30 -0500

    Day 8 refactor

--- 8.js
+++ 8.js
@@ -1,7 +1,8 @@
 const input = require('./8-input.json')[0];
-const testInput = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
 
 const getArr = (str) => str.split(' ').map(i => Number(i));
+const getHeader = (arr, i) => [arr[i], arr[i+1]];
+const getMetadata = (arr, i, count) => arr.slice(i + 2, i + 2 + count);
 const sum = (a, b) => a + b;
 
 function part1(str) {
@@ -10,55 +11,51 @@ function part1(str) {
   let position = 0;
 
   const increaseTotal = (start, entryCount) => {
-    total += arr.slice(start + 2, start + 2 + entryCount).reduce(sum);
+    total += getMetadata(arr, start, entryCount).reduce(sum);
   }
 
-  const getMetadata = (index) => {
-    let children = arr[index];
-    let entryCount = arr[index+1];
+  const sumMetadata = (index) => {
+    let [children, entryCount] = getHeader(arr, index);
 
     if (children === 0) {
       increaseTotal(index, entryCount);
       position += entryCount;
       return;
     }
+
     for (let i = 0; i < children; i++) {
       position+=2;
-      getMetadata(position);
+      sumMetadata(position);
     }
+
     increaseTotal(position, entryCount);
     position += entryCount;
+    return total;
   }
-  getMetadata(0);
-  return total;
+
+  return sumMetadata(0);
 }
-// console.log(part1(testInput));
 // console.log(part1(input));
 
 function part2(str) {
   let arr = getArr(str);
   let position = 0;
 
-  const getEntries = (start, entryCount) => {
-    return arr.slice(start + 2, start + 2 + entryCount);
-  }
-
-  const getMetadata = (index) => {
-    let children = arr[index];
-    let entryCount = arr[index + 1];
+  const getNodeValue = (index) => {
+    let [children, entryCount] = getHeader(arr, index);
     let childMetadata = [];
 
     if (children === 0) {
       position += entryCount;
-      return getEntries(index, entryCount);
+      return getMetadata(arr, index, entryCount);
     }
 
     for (let i = 0; i < children; i++) {
       position += 2;
-      childMetadata.push(getMetadata(position).reduce(sum));
+      childMetadata.push(getNodeValue(position).reduce(sum));
     }
 
-    let val = getEntries(position, entryCount).map(p => {
+    let val = getMetadata(arr, position, entryCount).map(p => {
       if (p > children || p === 0) return 0;
       return childMetadata[p-1];
     }).reduce(sum);
@@ -66,9 +63,8 @@ function part2(str) {
     position += entryCount;
     return [val];
   }
-  return getMetadata(0)[0];
+  return getNodeValue(0)[0];
 }
-// console.log(part2(testInput));
 // console.log(part2(input));
 
 module.exports = {
